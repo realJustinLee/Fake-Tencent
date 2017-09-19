61fb11521de995200c2304ff9b4a3542ea343596
李欣 <JustinDellAdam@live.com>
2017-09-19 18:52:55 +0800

    macOS Client INIT

---
+++ macOS Version/Fake Tencent/Fake Tencent/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Fake Tencent
+//
+//  Created by 李欣 on 2017/9/19.
+//  Copyright © 2017年 李欣. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
